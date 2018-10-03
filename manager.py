22e6c13a08eae6a62625be21ee0e1362af7cd4af
Dan Bourke <dbourke@atlassian.com>
2018-10-04 09:22:56 +1000

    remove default path for #opsec reasons

--- manager.py
+++ manager.py
@@ -373,11 +373,12 @@ def get_permission_stuff():
 def get_spacecrab_path():
     print("\nWe'll need a path, which will be part of the generated token's ARNs.")
     print("The path must start and end with /")
+    print("Consider *not* using /SpaceCrab/ or anything like it.\ni.e. make this something plausible if attackers read the path.")
     path = raw_input(
-        'Please enter a "Path" for your users (defaults to /SpaceCrab/): '
+        'Please enter a "Path" for your users\: '
         ).strip()
     if len(path) == 0:
-        path = "/SpaceCrab/"
+        path = get_spacecrab_path()
     if path[0] != '/':
         path = '/' + path
     if path[-1] != '/':
